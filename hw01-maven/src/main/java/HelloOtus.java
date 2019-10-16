dac7979610c6a971a4dca2230415b48c5374750d
Mikhail <michail.metlin@smartbics.com>
2019-10-16 18:33:15 +0300

    Пустой проект maven + модуль hw01-maven

---
+++ hw01-maven/src/main/java/HelloOtus.java
@@ -0,0 +1,11 @@
+import com.google.common.base.Splitter;
+import java.util.List;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        List<String> wordList = Splitter.on('+')
+                .splitToList("hello+otus+java");
+
+        System.out.println(wordList);
+    }
+}
