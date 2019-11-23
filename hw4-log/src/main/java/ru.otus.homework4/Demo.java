e1a97e0f3679b3d3711c3cd8bdd4489e7ca2cf29
Mikhail <michail.metlin@smartbics.com>
2019-11-23 15:37:13 +0300

    hw4: Автоматическое логирование (новая версия)

--- hw4-log/src/main/java/ru.otus.homework4/Demo.java
+++ hw4-log/src/main/java/ru.otus.homework4/Demo.java
@@ -2,10 +2,9 @@ package ru.otus.homework4;
 
 public class Demo {
     public static void main(String[] args) {
-        new TestLogging().calculation(5);
-        new TestLogging().calculation(15);
-        new TestLogging().calculation(3);
-
-        new TestLogging().getNumber(1);
+        Logging logging = IoC.createMyClass();
+        logging.calculation(7);
+        logging.getNumber(1);
+        logging.getParam(5);
     }
 }
--- hw4-log/src/main/java/ru.otus.homework4/TestLogging.java
+++ hw4-log/src/main/java/ru.otus.homework4/TestLogging.java
@@ -1,37 +1,18 @@
 package ru.otus.homework4;
 
-import java.lang.reflect.Method;
-
-public class TestLogging {
-
-    private int param;
-    private String methodName;
-    private final String EMPTY_STRING = "";
+public class TestLogging implements Logging {
 
     @Log
-    public int calculation(int param) {
-        this.param = param;
-        action();
-        return param;
+    @Override
+    public void calculation(int param) {
     }
 
-    public int getNumber(int number) {
-        return number;
+    @Override
+    public void getNumber(int number) {
     }
 
-    private void action() {
-        try {
-            methodName = getLogMethod(TestLogging.class.getMethod("calculation", int.class));
-        } catch (NoSuchMethodException e) {
-            e.getMessage();
-        }
-
-        if (!methodName.equals(EMPTY_STRING)) {
-            System.out.println("executed method: " + methodName + " ,param: " + param);
-        }
-    }
-
-    private String getLogMethod(Method method) {
-        return method.isAnnotationPresent(Log.class) ? method.getName() : EMPTY_STRING;
+    @Log
+    @Override
+    public void getParam(int param) {
     }
 }
